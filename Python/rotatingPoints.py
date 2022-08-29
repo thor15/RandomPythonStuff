6ef0b13eca08c1e496f30c56207910b785c7ec38
thor15 <44789309+thor15@users.noreply.github.com>
2022-08-29 11:17:15 -0500

    Update with more Attempts

--- Python/rotatingPoints.py
+++ Python/rotatingPoints.py
@@ -3,20 +3,58 @@ from PIL import Image
 WIDTH = 800
 ARRAYLENG= WIDTH*WIDTH
 VALUE = [0]*(ARRAYLENG)
+SIDE1X = []
+SIDE1Y = []
+SIDE2X = []
+SIDE2Y = []
+TEST = []
+COUNTER = []
 
 
 def points(x, y):
-    for i in range(int(x)-4, int(x)+4,1):
-        for j in range(int(y)-4, int(y)+4,1):
-            VALUE[(399-i)*WIDTH+399-j]=40
+    VALUE[(399+round(x))*WIDTH+399+round(y)] = 40
+    if(not ((399+round(x))*WIDTH+399+round(y)) in TEST):
+        TEST.append((399+round(x))*WIDTH+399+round(y))
+    COUNTER.append(0)
 
+
+def lerp(low, high, t):
+    return low*(1-t)+high*t
+
+
+def drawSideHorizantal(ax, ay, bx, by):
+    distance = sqrt(pow(bx-ax, 2)+pow(by-ay,2))
+    for i in range(0, int(distance), 1):
+        x = lerp(ax, bx, i/distance)
+        y = lerp(ay, by, i/distance)
+        points(round(x), round(y))
+
+
+def drawSideVertical(ax, ay, bx, by, n):
+    distance = sqrt(pow(bx-ax, 2)+pow(by-ay,2))
+    for i in range(0, int(distance), 1):
+        x = lerp(ax, bx, i/distance)
+        y = lerp(ay, by, i/distance)
+        points(round(x), round(y))
+        if(n == 0):
+            SIDE1X.append(x)
+            SIDE1Y.append(y)
+        else:
+            SIDE2X.append(x)
+            SIDE2Y.append(y)
+    
 
 #starting points a:(-100, 50sqrt(3)), b: (100, 50sqrt(3)), c: (0,-50sqrt(3))
 def calculatePoints(angle, radius):
-    rAngle = angle*pi/2
+
+    #calculate angles
+    rAngle = angle*pi/180
     shiftB = acos(100/radius)
     shiftA = asin(-100/radius)
+    # print(rAngle, shiftB, shiftA)
+    # print(cos(2*pi+rAngle-shiftA-pi/2))
 
+    # set coordinates
     ax = radius*cos(2*pi-rAngle+shiftA-pi/2)
     ay = radius*sin(2*pi-rAngle+shiftA+pi/2)
     bx = radius*cos(rAngle+shiftB)
@@ -24,20 +62,91 @@ def calculatePoints(angle, radius):
     cx = radius*sin(rAngle)
     cy = -1*radius*cos(rAngle)
 
-    print(ax, ay)
-    print(bx, by)
-    print(cx, cy)
-    points(ax, ay)
-    points(bx, by)
-    points(cx, cy)
+    #draw side
+    drawSideHorizantal(ax, ay, bx, by)
+    drawSideVertical(ax, ay, cx, cy, 0)
+    drawSideVertical(bx, by, cx, cy, 1)
 
 radius = sqrt(pow(100,2)+pow(50*sqrt(3), 2))
-calculatePoints(90, radius)
-img = Image.new(mode="RGB", size=(WIDTH, WIDTH))
 
-for x in range(0, WIDTH-1, 1):
-    for y in range(0, WIDTH, 1):
-        img.putpixel([x,y], (abs(int(VALUE[x*WIDTH+y])),abs(int(VALUE[x*WIDTH+y])),abs(int(VALUE[x*WIDTH+y]))*255))
+# VALUE = [0]*(ARRAYLENG)
+# SIDE1X = []
+# SIDE1Y = []
+# SIDE2X = []
+# SIDE2Y = []
 
 
-img.show()
+# calculatePoints(45, radius)
+
+# #print(len(SIDE1X), len(SIDE1Y), len(SIDE2X), len(SIDE2Y))
+
+# for point in range(0, len(SIDE1X)-1, 1):
+#     #print("A:", "(" +str(SIDE1X[point]) + ","+ str(SIDE1Y[point]) +")", "   B:", "(" +str(SIDE2X[point]) + ","+ str(SIDE2Y[point]) +")")
+#     drawSideHorizantal(SIDE1X[point], SIDE1Y[point], SIDE2X[point], SIDE2Y[point])
+
+# print(len(COUNTER), len(TEST))
+
+# img = Image.new(mode="RGB", size=(WIDTH, WIDTH))
+
+# for x in range(0, WIDTH-1, 1):
+#     for y in range(0, WIDTH, 1):
+#         count = 0
+#         if(not VALUE[(x-1)*WIDTH+y] == 0):
+#             count = count + 1
+#         if(not VALUE[(x+1)*WIDTH+y] == 0):
+#             count = count + 1
+#         if(not VALUE[(x)*WIDTH+y-1] == 0):
+#             count = count + 1
+#         if(not VALUE[(x)*WIDTH+y+1] == 0):
+#             count = count + 1
+#         if(count > 3):
+#             VALUE[x*WIDTH+y] = 40
+#         img.putpixel([x,y], (abs(int(VALUE[x*WIDTH+y])),abs(int(VALUE[x*WIDTH+y])),abs(int(VALUE[x*WIDTH+y]))*255))
+
+
+# print(angle)
+# fileN = 'rotating' + str(angle) +  '.png'
+# img.save(fileN)
+#img.show()
+
+
+#for printing a cycle
+for angle in range(0, 360, 1):
+
+    VALUE = [0]*(ARRAYLENG)
+    SIDE1X = []
+    SIDE1Y = []
+    SIDE2X = []
+    SIDE2Y = []
+
+
+    calculatePoints(angle, radius)
+
+    #print(len(SIDE1X), len(SIDE1Y), len(SIDE2X), len(SIDE2Y))
+
+    for point in range(0, len(SIDE1X)-1, 1):
+        #print("A:", "(" +str(SIDE1X[point]) + ","+ str(SIDE1Y[point]) +")", "   B:", "(" +str(SIDE2X[point]) + ","+ str(SIDE2Y[point]) +")")
+        drawSideHorizantal(SIDE1X[point], SIDE1Y[point], SIDE2X[point], SIDE2Y[point])
+
+
+    img = Image.new(mode="RGB", size=(WIDTH, WIDTH))
+
+    for x in range(0, WIDTH-1, 1):
+        for y in range(0, WIDTH, 1):
+            count = 0
+            if(not VALUE[(x-1)*WIDTH+y] == 0):
+                count = count + 1
+            if(not VALUE[(x+1)*WIDTH+y] == 0):
+                count = count + 1
+            if(not VALUE[(x)*WIDTH+y-1] == 0):
+                count = count + 1
+            if(not VALUE[(x)*WIDTH+y+1] == 0):
+                count = count + 1
+            if(count > 3):
+                VALUE[x*WIDTH+y] = 40
+            img.putpixel([x,y], (abs(int(VALUE[x*WIDTH+y])),abs(int(VALUE[x*WIDTH+y])),abs(int(VALUE[x*WIDTH+y]))*255))
+
+
+    print(angle)
+    fileN = 'rotating' + str(angle) +  '.png'
+    img.save(fileN)
